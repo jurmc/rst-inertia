886906df1ae6c953add585cb2588beed6e1c33a7
Michal Jurek <jurmcc@gmail.com>
2024-10-06 21:58:07 +0200

    wip

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,6 @@
 extern crate sdl2;
 extern crate cairo;
 
-use sdl2::pixels::Color;
 use sdl2::event::Event;
 use sdl2::keyboard::Keycode;
 use sdl2::pixels::PixelFormatEnum;
@@ -11,6 +10,39 @@ use cairo::{ ImageSurface, Format, Context };
 
 use std::f64::consts::PI;
 use std::time::Duration;
+
+pub fn my_shape(ctx: &Context) {
+    ctx.set_source_rgb(1.0, 1.0, 1.0);
+    ctx.paint();
+
+    ctx.set_source_rgba(0.1, 0.1, 7.0, 0.95);
+    ctx.move_to(-75.0, 0.0);
+    ctx.line_to(0.0, 0.0);
+    ctx.line_to(0.0, -75.0);
+
+    ctx.move_to(10.0, 10.0);
+    ctx.line_to(75.0, 75.0);
+
+    // Arcs
+    ctx.move_to(0.0, -75.0);
+    ctx.new_sub_path();
+    ctx.arc(0.0, 0.0, 75.0, 180.0 * (PI / 180.0), 270.0 * (PI / 180.0));
+
+    ctx.set_line_width(10.0);
+    ctx.stroke().unwrap();
+
+    ctx.set_source_rgba(1.0, 0.2, 0.2, 0.5);
+    ctx.new_sub_path();
+    ctx.arc(0.0, -75.0, 20.0, 0.0, 2.0 * PI);
+    ctx.new_sub_path();
+    ctx.arc(-75.0, 0.0, 20.0, 0.0, 2.0 * PI);
+    ctx.fill().unwrap();
+
+    // Bezier
+    ctx.move_to(-75.0, 75.0);
+    ctx.curve_to(0.0, 75.0, 0.0, -75.0, 75.0, -75.0);
+    ctx.stroke().unwrap();
+}
 
 pub fn main() {
     let sdl_ctx = sdl2::init().unwrap();
@@ -39,45 +71,21 @@ pub fn main() {
         dest_surface = ImageSurface::create_for_data_unsafe((*(sdl_surface.raw())).pixels as *mut u8, Format::ARgb32, width, height, pitch)
             .expect("Couldn't create Cairo surface (using pixels from SDL surface)");
     };
-    let ctx = Context::new(&dest_surface).unwrap();
+    let mut ctx = Context::new(&dest_surface).unwrap();
 
-    //SDL_SetRenderDrawColor(renderer, 0, 0, 0, 0); // TODO: do we have anythign useful from it?
-    //SDL_RenderClear(renderer); // TODO: do we have anythign useful from it?
+    let (width, height) = (width as f64, height as f64);
 
-    // Lines
-    ctx.set_source_rgb(1.0, 1.0, 1.0);
-    ctx.paint();
-
-    ctx.set_source_rgba(0.1, 0.1, 7.0, 0.95);
-    ctx.move_to(150.0 - 75.0, 150.0);
-    ctx.line_to(150.0, 150.0);
-    ctx.line_to(150.0, 150.0 - 75.0);
-
-    ctx.move_to(150.0 + 10.0, 150.0 + 10.0);
-    ctx.line_to(150.0 + 75.0, 150.0 + 75.0);
-
-    ctx.move_to(150.0, 150.0 - 75.0);
-    ctx.new_sub_path();
-    ctx.arc(150.0, 150.0, 75.0, 180.0 * (PI / 180.0), 270.0 * (PI / 180.0));
-
-    ctx.set_line_width(10.0);
-    ctx.stroke().unwrap();
-
-    ctx.set_source_rgba(1.0, 0.2, 0.2, 0.5);
-    ctx.new_sub_path();
-    ctx.arc(150.0, 150.0 - 75.0, 20.0, 0.0, 2.0 * PI);
-    ctx.new_sub_path();
-    ctx.arc(150.0 - 75.0, 150.0, 20.0, 0.0, 2.0 * PI);
-    ctx.fill();
+    ctx.translate(width / 2.0, height / 2.0);
+    my_shape(&ctx);
 
     let texture_creator = canvas.texture_creator();
     let new_texture = texture_creator.create_texture_from_surface(sdl_surface).unwrap();
     canvas.copy(&new_texture,None,None).unwrap();
-    canvas.present();
 
     let mut event_pump = sdl_ctx.event_pump().unwrap();
     let mut i = 0;
     'running: loop {
+        canvas.present();
         i = (i + 1) % 255;
         for event in event_pump.poll_iter() {
             match event {
